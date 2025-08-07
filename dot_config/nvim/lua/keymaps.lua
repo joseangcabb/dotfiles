7d9445e5ab554fbb5f0143dc3908dd38bd7d27fe
Jose Angel <joseangcabb@gmail.com>
2025-08-07 15:36:52 -0500

    Add nvim keymap to copy path of a file

--- dot_config/nvim/lua/keymaps.lua
+++ dot_config/nvim/lua/keymaps.lua
@@ -1,2 +1,6 @@
 -- Select all (normal mode).
 vim.api.nvim_set_keymap("n", "<C-a>", "ggVG", { noremap = true, silent = true })
+
+-- Copy absolute file path to system clipboard.
+vim.api.nvim_set_keymap(
+  "n", "<C-c>", ":let @+ = expand('%:p')<CR>", { noremap = true, silent = true })
