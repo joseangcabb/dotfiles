1180e9c35faac403fda89da4015ccf8ce94003dc
Jose Angel <joseangcabb@gmail.com>
2025-08-21 19:59:50 -0500

    chore(config): add mason-null-ls for automatic tool installation

---
+++ dot_config/nvim/lua/plugins/formatting/mason_null_ls.lua
@@ -0,0 +1,16 @@
+return {
+	"jay-babu/mason-null-ls.nvim",
+	dependencies = {
+		"williamboman/mason.nvim",
+		"nvimtools/none-ls.nvim",
+	},
+	config = function()
+		require("mason-null-ls").setup({
+			ensure_installed = {
+				"prettierd",
+			},
+			run_on_start = true,
+			auto_update = true,
+		})
+	end,
+}
